0a5dee25a9e4a21c523382427b9529631d8c1815
thaian9797 <76254938+thaian9797@users.noreply.github.com>
2020-12-19 21:26:03 +0700

    Add files via upload

---
+++ Problem 0/Problem 0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math as m
+
+x = float(input('Enter number x: '))
+y= float(input('Enter number y: '))
+
+print(x**y)
+print(m.log2(x))
+
